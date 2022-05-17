5c07efec7de076d9b8fb1b0fbe709dca70963254
emmanueles1993 <emmanueles1993@hotmail.com>
2022-05-17 18:20:11 -0500

    footersection

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "./Footer.css";
+import { FaHandPaper } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer class="footer container">
-      <p class="footer__p">VASED</p>
+      <div className="logo_text">
+        <p class="footer__p">VASED</p>
+        <div className="icons_objects ">
+          <FaHandPaper />
+        </div>
+      </div>
       <div class="footer_social_networks">
-        <i class="bi bi-facebook"></i>
-        <i class="bi bi-twitter"></i>
-        <i class="bi bi-instagram"></i>
+        <i class="bi bi-facebook social"></i>
+        <i class="bi bi-twitter social"></i>
+        <i class="bi bi-instagram social"></i>
       </div>
       <div class="paragraph_container">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et nulla illo numquam? Est modi harum ullam odit rem maiores perferendis quidem non? Deleniti nam molestiae cum provident eum, qui natus.</p>
-        </div>
+        <p class="text_footer">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et nulla
+          illo numquam? Est modi harum ullam odit rem maiores perferendis quidem
+      
+        </p>
+      </div>
     </footer>
   );
 };
