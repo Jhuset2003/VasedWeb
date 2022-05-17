7c787d7849c82ba65d2a3467b6628304c004b6af
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-05-17 07:05:36 -0500

    progress with login functionality

--- src/components/Forms/FormLogin.jsx
+++ src/components/Forms/FormLogin.jsx
@@ -2,37 +2,49 @@ import React from 'react'
 import '../../styles/Inputs.css';
 import './FormLogin.css';
 import '../../styles/Buttons_module.css';
+import { Formik } from 'formik';
 
 const FormLogin = () => {
   return (
-    <div className="form-container">
-        <h1 className="form-title">Iniciar sesión</h1>
-            <form action="" className="form">
-                <div className="form-item">
-                    <label htmlFor="" className="form-title">Usuario</label>
-                    <input 
-                    type="text" 
-                    id="userName" 
-                    name="userName" 
-                    placeholder="Barbablanca"
-                    className="purple-input"/>
-                </div>
+    <>
+    <Formik
+    onSubmit={()=>{
+      console.log("formulario enviado")
+    }}>
+      {({handleSubmit, handleChange}) => (
+        <div className="form-container">
+            <h1 className="form-title">Iniciar sesión</h1>
+                <form action="" className="form" onSubmit={handleSubmit}>
+                    <div className="form-item">
+                        <label htmlFor="user" className="form-title">Usuario</label>
+                        <input 
+                        type="text" 
+                        id="user" 
+                        name="user" 
+                        placeholder="Barbablanca"
+                        className="purple-input"
+                        onChange={handleChange}/>
+                    </div>
 
-                <div className="form-item">
-                    <label htmlFor="" className="form-title">Contraseña</label>
-                    <input 
-                    type="password" 
-                    id="password" 
-                    name="password" 
-                    placeholder="Contraseña123..."
-                    className="purple-input"/>
-                </div>
-                <button 
-                type="submit" 
-                className='BtnPurple'>Entrar</button>
-            </form> 
-            <h3 className="form-recover">¿Olvidaste tu contraseña?</h3>
-    </div>
+                    <div className="form-item">
+                        <label htmlFor="password" className="form-title">Contraseña</label>
+                        <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Contraseña123..."
+                        className="purple-input"
+                        onChange={handleChange}/>
+                    </div>
+                    <button 
+                    type="submit" 
+                    className='BtnPurple'>Entrar</button>
+                </form> 
+                <a href="#" className="form-recover">¿Olvidaste tu contraseña?</a>
+        </div>
+      )}
+    </Formik>
+  </>
   )
 }
 
