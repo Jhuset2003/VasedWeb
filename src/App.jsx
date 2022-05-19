adb3208d2a81f08975d96b84b3de1f1f18775d04
emmanueles1993 <emmanueles1993@hotmail.com>
2022-05-19 15:20:58 -0500

    landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import NavBarLogin from './components/NavBars/NavBarLogin/NavBarLogin'
-import Home from './pages/Home'
-import Aulas from './pages/Aulas'
-import Activities from './pages/Activities'
-import AdminUser from './pages/AdminUsers'
-import Profile from './pages/Profile'
+import NavBarLogin from "./components/NavBars/NavBarLogin/NavBarLogin";
+import Aulas from "./pages/Aulas";
+import Activities from "./pages/Activities";
+import AdminUser from "./pages/AdminUsers";
+import Profile from "./pages/Profile";
 
-import './styles/App.css'
-import Footer from './components/Sections/Footer'
- 
+import "./styles/App.css";
+import Footer from "./components/Sections/Footer";
+import Landing from "./pages/Landing";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <NavBarLogin/>
+      <NavBarLogin />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/aulas" element={<Aulas/>}/>
-        <Route path="/Actividades" element={<Activities/>}/>
-        <Route path="/usuarios" element={<AdminUser/>}/>
-        <Route path="/perfil" element={<Profile/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/aulas" element={<Aulas />} />
+        <Route path="/Actividades" element={<Activities />} />
+        <Route path="/usuarios" element={<AdminUser />} />
+        <Route path="/perfil" element={<Profile />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 export default App;
-
