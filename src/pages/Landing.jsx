7d954fe3dfe50e71043aa13057974cdc55e62a38
emmanueles1993 <emmanueles1993@hotmail.com>
2022-06-01 09:42:25 -0500

    hero section last changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Hero from "../components/Sections/Hero";
-import FormAdd from "../components/Forms/FormAdd";
 
 const Landing = () => {
   return (
     <div>
       <Hero />
-       <FormAdd />
     </div>
   );
 };
